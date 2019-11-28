460e64ac34d5bd9d26b933bb049a38880ada10b1
paddy-7385 <pandurangmatkar4@gmail.com>
2019-11-28 11:53:13 +0530

    class foucs

--- Hello.tsx
+++ Hello.tsx
@@ -8,16 +8,23 @@ export default class Hello extends Component {
       text: ""
     }
     this.setText = this.setText.bind(this);
+    this.onClicked = this.onClicked.bind(this);
+    this.inputHandler = React.createRef();
   }
 
   setText(event){
     this.setState({text: event.target.value})    
   }
 
-  render() {
+  onClicked(){
+    this.inputHandler.current.focus();
+  }
+
+  render() {    
     return(
       <div>
-        <input name= "inputText" onChange = {this.setText} ></input>
+        <input type= "text"  ref={this.inputHandler} onChange = {this.setText}></input>
+        <input type= "button" onClick = {this.onClicked}  Value= "Click me" ></input>
         <div name ="outputText">{this.state.text}</div>
       </div>
     )
